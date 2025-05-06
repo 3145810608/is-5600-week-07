7729ea2a80cb57abeffb61d443fed49a105a5ffe
3145810608 <kranthikumar.duggirala@slu.edu>
2025-05-06 05:14:53 +0000

    Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { Route, Routes} from 'react-router-dom';
-
-import Header from './components/Header';
-import CardList from './components/CardList';
-import SingleView from './components/SingleView';
-import productData from './data/full-products';
-
+import { useEffect, useState } from "react";
+import CardList from "./components/CardList";
+import Cart from "./components/Cart";
+import Orders from "./components/Orders";
 
 function App() {
-  
+  const [products, setProducts] = useState([]);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/products")
+      .then((res) => res.json())
+      .then(setProducts);
+
+    fetch("http://localhost:3001/orders")
+      .then((res) => res.json())
+      .then(setOrders);
+  }, []);
+
   return (
-    <div className="App">
-      <Header />
-      
-        <Routes>
-          <Route path="/" element={<CardList data={productData} />} />
-          <Route path="/product/:id" element={<SingleView data={productData} />} />
-        </Routes>
-      
+    <div className="pa4">
+      <h1 className="f2">Fullstack Prints</h1>
+      <CardList products={products} />
+      <Cart onOrderPlaced={(newOrder) => setOrders([...orders, newOrder])} />
+      <Orders orders={orders} />
     </div>
   );
 }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,65 +1,39 @@
-import React, { useContext } from 'react';
-import PurchaseForm from './PurchaseForm';
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
-const Cart = () => {
-  // TODO - get cart items from context
-  const cartItems = [];
-  const removeFromCart = () => {};
-  const updateItemQuantity = () => {};
-  const getCartTotal = () => {};
+const Cart = ({ onOrderPlaced }) => {
+  const { cart, clearCart } = useContext(CartContext);
+
+  const placeOrder = () => {
+    fetch("http://localhost:3001/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: cart }),
+    })
+      .then((res) => res.json())
+      .then((newOrder) => {
+        clearCart();
+        onOrderPlaced(newOrder);
+      });
+  };
 
   return (
-    <div className="center mw7 mv4">
-      <div className="bg-white pa3 mb3">
-        <h2 className="f2 mb2">Cart</h2>
-        <table className="w-100 ba pa2">
-          <thead>
-            <tr>
-              <th className="tl pv2">Product</th>
-              <th className="tr pv2">Quantity</th>
-              <th className="tr pv2">Price</th>
-              <th className="tr pv2">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartItems && cartItems.map((item) => (
-              <tr key={item._id}>
-                <td className="tl pv2">{item.description}</td>
-                <td className="tr pv2">
-                  <a
-                    className="pointer ba b--black-10 pv1 ph2 mr2"
-                    onClick={() => updateItemQuantity(item._id, -1)}
-                  >
-                    -
-                  </a>
-                  {item.quantity}
-                  <a
-                    className="pointer ba b--black-10 pv1 ph2 ml2"
-                    onClick={() => updateItemQuantity(item._id, 1)}
-                  >
-                    +
-                  </a>
-                </td>
-                <td className="tr pv2">${item.price * item.quantity}</td>
-                <td className="tr pv2">
-                  <a
-                    className="pointer ba b--black-10 pv1 ph2"
-                    onClick={() => removeFromCart(item)}
-                  >
-                    Remove
-                  </a>
-                </td>
-              </tr>
+    <div className="mt4">
+      <h2 className="f3">Cart</h2>
+      {cart.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <>
+          <ul>
+            {cart.map((item, index) => (
+              <li key={index}>{item.name} - ${item.price}</li>
             ))}
-          </tbody>
-        </table>
-        <div className="tr f4 mv3">
-          Total: ${getCartTotal().toFixed(2)}
-        </div>
-      </div>
-      <div className="flex justify-end pa3 mb3">
-        <PurchaseForm />
-      </div>
+          </ul>
+          <button className="bg-green white pa2 br2 mt2" onClick={placeOrder}>
+            Place Order
+          </button>
+        </>
+      )}
     </div>
   );
 };
